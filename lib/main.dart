7703b33b9b74046904fc95351613034a0be97419
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-08-31 20:20:45 +0300

    code_architecture_fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:food_delivery_app/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(create: (context) => ThemeProvider(),
-    child: const MyApp(),)
-  );
-} 
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,28 +1,24 @@
-
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/theme/dark_mode.dart';
 import 'package:food_delivery_app/theme/light_mode.dart';
 
-class ThemeProvider extends ChangeNotifier{
+class ThemeProvider extends ChangeNotifier {
   ThemeData _themeData = lightMode;
 
   ThemeData get themeData => _themeData;
 
   bool get isDarkMode => _themeData == darkMode;
 
-  set themeData(ThemeData themeData){
+  set themeData(ThemeData themeData) {
     _themeData = themeData;
     notifyListeners();
   }
 
-
-  void toogleTheme(){
-    if(_themeData == lightMode){
+  void toogleTheme() {
+    if (_themeData == lightMode) {
       themeData = darkMode;
-    }
-    else{
+    } else {
       themeData = lightMode;
     }
   }
-
 }
