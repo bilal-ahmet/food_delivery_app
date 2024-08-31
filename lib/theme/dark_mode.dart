285d7fdd246789c799e55a9a32745414b10cd3d9
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-08-31 17:22:21 +0300

    code_light_and_dark_mode_class

---
+++ lib/theme/dark_mode.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: const Color.fromARGB(255, 20, 20, 20),
+    primary: const Color.fromARGB(255, 120, 120, 120),
+    secondary: const Color.fromARGB(255, 30, 30, 30),
+    tertiary: const Color.fromARGB(255, 47, 47, 47),
+    inversePrimary: Colors.grey.shade300
+  )
+);
