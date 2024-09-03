f0fb07164817d825f831af0658973b9c79b126a6
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-09-03 14:19:26 +0300

    current_location_informatin_and_sliver_app_bar_configuraitons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_current_location.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
+import 'package:food_delivery_app/components/my_sliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +14,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Home"),
-        ),
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        drawer: const MyDrawer());
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      drawer: const MyDrawer(),
+      body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                MySliverAppBar(
+                    title: const Text("title"),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Divider(indent: 25, endIndent: 25, color: Theme.of(context).colorScheme.secondary,),
+                        const MyCurrentLocation()
+                      ],
+                    ))
+              ],
+          body: Container(
+            color: Colors.blue,
+          )),
+    );
   }
 }
