4f5b41cdd51ff616dd4f46e369d539fa5c63234f
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-09-04 13:20:30 +0300

    code_tab_bar_menu_items_methode

--- lib/components/my_tab_bar.dart
+++ lib/components/my_tab_bar.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/models/food.dart';
 
 class MyTabBar extends StatelessWidget {
   final TabController tabController;
   const MyTabBar({super.key, required this.tabController});
 
+  List<Tab> _buildCategoryTabs() {
+    return FoodCategory.values.map((toElement) {
+      return Tab(
+        text: toElement.toString().split(".").last,
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TabBar(
-        controller: tabController, 
-        tabs: const [
-          Tab(icon: Icon(Icons.home),),
-          Tab(icon: Icon(Icons.settings),),
-          Tab(icon: Icon(Icons.person),),
-        ]),
+      child: TabBar(controller: tabController, tabs: _buildCategoryTabs()),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_current_location.dart';
 import 'package:food_delivery_app/components/my_description_box.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
 import 'package:food_delivery_app/components/my_sliver_app_bar.dart';
 import 'package:food_delivery_app/components/my_tab_bar.dart';
+import 'package:food_delivery_app/models/food.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,19 +15,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
-
   late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController =
+        TabController(length: FoodCategory.values.length, vsync: this);
   }
 
   @override
   void dispose() {
     super.dispose();
     _tabController.dispose();
+  }
+
+  List<Food> _filterMenuByCategory(FoodCategory category, List<Food> fullMenu){
+    return fullMenu.where((food) => food.foodCategory == category).toList();
+  }
+
+  List<Widget> getFoodInThisCategory(List<Food> fullMenu) {
+    return FoodCategory.values.map((category) {
+      List<Food> categoryMenu = _filterMenuByCategory(category, fullMenu);
+
+      return ListView.builder(
+        itemCount: categoryMenu.length,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(categoryMenu[index].name),
+          );
+        },
+      );
+    }).toList();
   }
 
   @override
@@ -40,13 +62,23 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Divider(indent: 25, endIndent: 25, color: Theme.of(context).colorScheme.secondary,),
+                        Divider(
+                          indent: 25,
+                          endIndent: 25,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
                         const MyCurrentLocation(),
                         const MyDescriptionBox()
                       ],
                     ))
               ],
-          body: TabBarView(controller: _tabController, children: const [Text("hello"), Text("hello"), Text("hello")])),
+          body: TabBarView(controller: _tabController, children: const [
+            Text("hello"),
+            Text("hello"),
+            Text("hello"),
+            Text("hello"),
+            Text("hello")
+          ])),
     );
   }
 }
