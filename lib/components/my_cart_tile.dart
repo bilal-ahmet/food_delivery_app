c24e041fc72c6dfd8bbf945c8a23f68da190e8f3
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-09-05 15:21:12 +0300

    completed_cart_page_food_quantity_clear_cart_tile_transactions_also

--- lib/components/my_cart_tile.dart
+++ lib/components/my_cart_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_quantity_selector.dart';
 import 'package:food_delivery_app/models/cart_items.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
 import 'package:provider/provider.dart';
@@ -11,10 +12,16 @@ class MyCartTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<Restaurant>(
       builder: (context, restaurant, child) => Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(8)),
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ClipRRect(
                     borderRadius: BorderRadius.circular(8),
@@ -34,8 +41,45 @@ class MyCartTile extends StatelessWidget {
                     Text("${cartItem.food.price} ₺"),
                   ],
                 ),
+                const Spacer(),
+                const SizedBox(
+                  width: 15,
+                ),
+                MyQuantitySelector(
+                    quantity: cartItem.quantity,
+                    food: cartItem.food,
+                    onIncrement: () {
+                      restaurant.addToCart(
+                          cartItem.food, cartItem.selectedAddons);
+                    },
+                    onDecrement: () {
+                      restaurant.removeFromCart(cartItem);
+                    })
               ],
             ),
+            SizedBox(
+              height: cartItem.selectedAddons.isEmpty ? 0 : 60,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: cartItem.selectedAddons
+                    .map((addon) => Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: FilterChip(
+                            label: Row(
+                              children: [
+                                Text(addon.name),
+                                Text(" (${addon.price} ₺)", style: TextStyle(color: Theme.of(context).colorScheme.primary),)
+                              ],
+                            ),
+                            onSelected: (value) {},
+                            backgroundColor: Theme.of(context).colorScheme.secondary,
+                            labelStyle: TextStyle(color: Theme.of(context).colorScheme.inversePrimary, fontSize: 12),
+                            shape: StadiumBorder(side: BorderSide(color: Theme.of(context).colorScheme.inversePrimary)),
+                          ),
+                    ))
+                    .toList(),
+              ),
+            )
           ],
         ),
       ),
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_button.dart';
 import 'package:food_delivery_app/components/my_cart_tile.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
 import 'package:provider/provider.dart';
@@ -17,18 +18,54 @@ class CartPage extends StatelessWidget {
             title: const Text("Cart"),
             backgroundColor: Colors.transparent,
             foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text(
+                            "Are you sure you want to clear the cart ?"),
+                        actions: [
+                          TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text("Cancel")),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                restaurant.clearCart();
+                              },
+                              child: const Text("Yes"))
+                        ],
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.delete))
+            ],
           ),
           body: Column(
             children: [
+
+
               Expanded(
-                  child: ListView.builder(
-                      itemCount: userCart.length,
-                      itemBuilder: (context, index) {
+                child: Column(
+                  children: [
+                    userCart.isEmpty ? const Expanded(child: Center(child: Text("Cart is empty"))) : Expanded(
+                        child: ListView.builder(
+                            itemCount: userCart.length,
+                            itemBuilder: (context, index) {
+                              final cartItem = userCart[index];
+                
+                              return MyCartTile(cartItem: cartItem);
+                            }))
+                  ],
+                ),
+              ),
 
-                        final cartItem = userCart[index];
-
-                        return MyCartTile(cartItem: cartItem);
-                      }))
+              MyButton(onTap: () {
+                
+              }, text: "Go to checkout"),
+              const SizedBox(height: 25,)
             ],
           ),
         );
