dd3927409579a813485a7644ec01165506831683
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-09-05 10:11:31 +0300

    completed_food_page

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -4,7 +4,12 @@ import 'package:food_delivery_app/models/food.dart';
 
 class FoodPage extends StatefulWidget {
   final Food food;
-  const FoodPage({super.key, required this.food});
+  final Map<Addon, bool> selectedAddons = {};
+  FoodPage({super.key, required this.food}) {
+    for (Addon addon in food.availableAddons) {
+      selectedAddons[addon] = false;
+    }
+  }
 
   @override
   State<FoodPage> createState() => _FoodPageState();
@@ -13,88 +18,116 @@ class FoodPage extends StatefulWidget {
 class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-                height: 400,
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset(
-                  widget.food.imagePath,
-                  fit: BoxFit.cover,
-                )),
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.food.name,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  Text("${widget.food.price} ₺",
-                      style: TextStyle(
+    return Stack(
+      children: [
+        Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                    height: 400,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset(
+                      widget.food.imagePath,
+                      fit: BoxFit.cover,
+                    )),
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.food.name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text("${widget.food.price} ₺",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Theme.of(context).colorScheme.primary)),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        widget.food.description,
+                        style: const TextStyle(
                           fontSize: 16,
-                          color: Theme.of(context).colorScheme.primary)),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    widget.food.description,
-                    style: const TextStyle(
-                        fontSize: 16,
                         ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Divider(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Add-ons",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).colorScheme.secondary),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          padding: EdgeInsets.zero,
+                          itemCount: widget.food.availableAddons.length,
+                          itemBuilder: (context, index) {
+                            Addon addon = widget.food.availableAddons[index];
+                            return CheckboxListTile(
+                              title: Text(addon.name),
+                              subtitle: Text(
+                                "${addon.price} ₺",
+                                style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary),
+                              ),
+                              value: widget.selectedAddons[addon],
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  widget.selectedAddons[addon] = value!;
+                                });
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Divider(
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    "Add-ons",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Theme.of(context).colorScheme.inversePrimary),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Theme.of(context).colorScheme.secondary),
-                        borderRadius: BorderRadius.circular(8)),
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      padding: EdgeInsets.zero,
-                      itemCount: widget.food.availableAddons.length,
-                      itemBuilder: (context, index) {
-                        Addon addon = widget.food.availableAddons[index];
-                        return CheckboxListTile(
-                          title: Text(addon.name),
-                          subtitle: Text("${addon.price} ₺", style: TextStyle(color: Theme.of(context).colorScheme.primary),),
-                          value: false,
-                          onChanged: (value) {},
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
+                ),
+                MyButton(onTap: () {}, text: "add to card"),
+                const SizedBox(
+                  height: 25,
+                )
+              ],
             ),
-            MyButton(onTap: () {}, text: "add to card"),
-            const SizedBox(height: 25,)
-          ],
+          ),
         ),
-      ),
+        SafeArea(
+            child: Opacity(
+          opacity: 0.6,
+          child: Container(
+            margin: const EdgeInsets.only(left: 25),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_rounded)),
+          ),
+        ))
+      ],
     );
   }
 }
